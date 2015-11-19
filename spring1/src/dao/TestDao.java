ac75e2f8f53bc1cca04ff578087adcaebd1db47b
Jimmy <Jimmy@192.168.2.3>
2015-11-19 17:55:59 +0800

    lonema  ...

--- spring1/src/dao/TestDao.java
+++ spring1/src/dao/TestDao.java
@@ -3,5 +3,6 @@ package dao;
 public class TestDao {
 	public void test(){
 		System.out.println("¶þ¸çÐÞ¸ÄµÄ........");
+		System.out.println("the first time test how to use git with my old brother...¹þ¹þ");
 	}
 }
